26cbc45b52f9ae4fd37ee87baaa842b744c3a663
Hector Miranda <hectorsucre13@gmail.com>
2022-05-10 10:52:32 -0500

    food movement

--- snake.py
+++ snake.py
@@ -41,17 +41,28 @@ def move():
 
     snake.append(head)
 
+
+
+
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
-    else:
+    else:   
         snake.pop(0)
+        if inside(food):
+            food.x += randrange(-1, 1) * 5
+            food.y += randrange(-1, 1) * 5
+        elif not inside(food):
+            food.x = randrange(-15, 15) * 10
+            food.y = randrange(-15, 15) * 10
+
 
     clear()
 
     for body in snake:
         square(body.x, body.y, 9, 'black')
+    
 
     square(food.x, food.y, 9, 'green')
     update()
