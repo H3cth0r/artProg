d410d48de069e1ef3a352c3f8c781aa03803186e
Hector Miranda <hectorsucre13@gmail.com>
2022-05-12 19:38:38 -0500

    some snake documentation

--- snake.py
+++ snake.py
@@ -1,18 +1,18 @@
-"""Game: Snake
- programmer 1: Luis Angel Gonzalez Tapia
- programmer 2: Héctor Miranda García
- Date: 11/05/2022
-"""
-
-
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
+"""Game:        Snake - Freegames
+Programmer1:    Luis Angel Gonzalez Tapia
+Programmar2:    Héctor Miranda García
+Date:           9 / may / 2022
+Description:    This is a replica of the popular retro game named
+                "Snake". Here the user is able to control the game
+                character movement; the objective is to make longer 
+                the snake by eating some of the food on the map.
+                The user wont be able to exit from the square map,
+                and wont be able also to cross its own tail.
+                For this activity we had to make the food move around
+                the map and not to go out of the map; also we had 
+                to make that the color of the head and the food to
+                change of color each time the game is started over, 
+                this in a random way.
 """
 
 from random import randrange, choice
@@ -20,13 +20,14 @@ from turtle import *
 
 from freegames import square, vector
 
-""" Created this list of colors for the snake and food"""
+# List of colors for head of snake and food
 Colores = ["green", "black", "blue", "yellow", "pink"]
+
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-ColorFood=choice(Colores) # random choice color from list for food
-ColorSnake=choice(Colores) # random choice color from list for snake
+ColorFood=choice(Colores)  # Random choice color from list for food
+ColorSnake=choice(Colores) # Random choice color from list for snake
 
 def change(x, y):
     """Change snake direction."""
@@ -56,22 +57,20 @@ def move():
     if head == food:
         print('Snake:', len(snake))
         
-        """Starting position of food"""
+        # Starting position of the food
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:   
         snake.pop(0)
         if inside(food):
-            """making foot move on random range
-            from -1 to 1
-            """
+            # This makes food move randomly, generating
+            # The movement of the food, goes from -1 to 1.
             food.x += randrange(-1, 1) * 5
             food.y += randrange(-1, 1) * 5
+
+        # In case food not inside the map, will be moved to
+        # Some plance inside the map.
         elif not inside(food):
-            """In case the food moves outside of
-            the map, the food will be place in
-            another ramdom place of the map.
-            """
             food.x = randrange(-15, 15) * 10
             food.y = randrange(-15, 15) * 10
 
@@ -90,9 +89,12 @@ setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+
+# Instanciating keys that produce movement of character
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+
 move()
 done()
